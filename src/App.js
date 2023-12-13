c03a109f3ab7916e4ab69c9d1830ff2e83c57da9
coderrob22 <robertknott7@gmail.com>
2023-12-13 09:51:07 -0500

    travel time layout complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  return(
+    <div className="app">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Logo />
+  <Form />
+  <PackingList />
+  <Stats />
     </div>
-  );
+  )
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Travel Time 🧳</h1>
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for the trip?</h3>
+    </div>
+  )
+}
+
+function PackingList() {
+  return <div className="list">List</div>
+}
+
+function Stats() {
+  return(
+    <footer className="stats">
+      <em> 
+      💼 You have x items in your list, and you have packed x(x%)
+      </em>
+    </footer>
+  )
+}
