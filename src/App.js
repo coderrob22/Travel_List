b5ea075182e5e8941f95d5b204ab8d9bda7c97c4
coderrob22 <robertknott7@gmail.com>
2024-01-03 09:51:07 -0500

    setting state management on packed items to delete, update, and add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  {id: 1, description: 'Passport', quantity: 2, packed: false},
-  {id: 2, description: 'Socks', quantity: 12, packed: false},
-  {id: 3, description: 'Toothbrush', quantity: 1, packed: true},
-];
-
 export default function App() {
   const [items, setItems] = useState([])
   
@@ -17,12 +11,20 @@ export default function App() {
     setItems(items => items.filter(item => item.id !== id))
   }
 
+  function handleToggleItem(id) {
+    setItems(items => items.map(item => item.id === id ? 
+      {...item, packed: !item.packed} :
+       item
+       )
+      );
+  }
+
   return(
     <div className="app">
 
   <Logo />
   <Form onAddItems={handleAddItems}/>
-  <PackingList items={items} onDeleteItem={handleDeleteItem}/>
+  <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
   <Stats />
     </div>
   )
@@ -79,15 +81,22 @@ function PackingList({ items, onDeleteItem }) {
   return (
   <div className="list">
     <ul >
-      {items.map(item=><Item item={item} onDeleteItem={onDeleteItem} key={item.id}/>)}
+      {items.map(item=>
+        <Item 
+          item={item} 
+          onDeleteItem={onDeleteItem} 
+          onToggleItem={onToggleItem} 
+          key={item.id}
+        />)}
     </ul>
   </div>
   )
 }
 
-function Item({item, onDeleteItem}){
+function Item({item, onDeleteItem, onToggleItem}){
   return (
   <li>
+    <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
     <span style={item.packed ? {textDecoration: "line-through"} : {}}>
       {item.quantity} {item.description}
     </span>
