9d26a278c377c0e46771fd90537f62696ca784b4
coderrob22 <robertknott7@gmail.com>
2024-01-05 09:51:07 -0500

    calculation functions added to react app

--- src/App.js
+++ src/App.js
@@ -106,6 +106,11 @@ function Item({item, onDeleteItem, onToggleItem}){
 }
 
 function Stats({ items }) {
+  if(!items.length) return
+  <p className="stats">
+    <em>Start adding some items to your packing list 🕺🏽</em>
+  </p>
+  
   const numItems = items.length
   const numPacked = items.filter((x) => x.packed).length
   const percentage = Math.round((numPacked / numItems) * 100)
