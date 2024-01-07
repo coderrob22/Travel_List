ee1ce6cbeddb58cd8a56abeffb04dd281417a683
coderrob22 <robertknott7@gmail.com>
2024-01-07 09:51:07 -0500

    project is functionally complete

--- src/App.js
+++ src/App.js
@@ -19,12 +19,21 @@ export default function App() {
       );
   }
 
+  function handleClearList(){
+    const confirm = window.confirm('Are you sure you want to clear the list?')
+    if (confirm) setItems([])
+  }
+
   return(
     <div className="app">
 
   <Logo />
   <Form onAddItems={handleAddItems}/>
-  <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
+  <PackingList 
+    items={items} 
+    onDeleteItem={handleDeleteItem} 
+    onClearItems={handleClearList}
+    onToggleItem={handleToggleItem}/>
   <Stats items={items} />
     </div>
   )
@@ -77,33 +86,44 @@ function Form({ onAddItems }) {
   )
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearItems }) {
 
   const [sortBy, setSortBy] = useState('input')
   
   let sortedItems;
+  
   if (sortBy === 'input') sortedItems = items;
-  if (sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
-  if (sortBy ==='packed') sortedItems = items.slice().sort((a,b)=> Number(a.packed) - Number(b.packed));
+  
+  if (sortBy === 'description') 
+    sortedItems = items
+    .slice()
+    .sort((a, b) => a.description
+    .localeCompare(b.description));
+  
+    if (sortBy ==='packed') sortedItems = items
+    .slice()
+    .sort((a,b)=> Number(a.packed) - Number(b.packed));
 
   return (
   <div className="list">
     <ul >
-      {sortedItems.map(item=>
+      {sortedItems.map((item)=>(
         <Item 
           item={item} 
           onDeleteItem={onDeleteItem} 
           onToggleItem={onToggleItem} 
           key={item.id}
-        />)}
+        />
+        ))}
     </ul>
 
     <div className="actions">
         <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value={input}>Sort by input order</option>
-          <option value={description}>Sort by description</option>
-          <option value={packed}>Sort by packed status</option>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearItems}>Clear List</button>
     </div>
   </div>
   )
@@ -122,10 +142,11 @@ function Item({item, onDeleteItem, onToggleItem}){
 }
 
 function Stats({ items }) {
-  if(!items.length) return
+  if(!items.length) return(
   <p className="stats">
     <em>Start adding some items to your packing list 🕺🏽</em>
   </p>
+  );
   
   const numItems = items.length
   const numPacked = items.filter((x) => x.packed).length
